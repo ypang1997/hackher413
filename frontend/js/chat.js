26dfac90ac05c3318a07fd40f49f69e9e27b53ce
ypang1997 <31942352+ypang1997@users.noreply.github.com>
2019-02-09 23:46:16 -0500

    lorem ipsum text arrays

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -2,6 +2,9 @@
 // If you use this channel ID your app will stop working in the future
 const CLIENT_ID = 'RNpL0ZhY5niTM8pl'; //created with my scaledrone account
 
+var fromArr = ["Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."];
+var toArr = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "];
+
 const drone = new ScaleDrone(CLIENT_ID, {
   data: { // Will be sent out as clientData via events
     name: getRandomName(),
