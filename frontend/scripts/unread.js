58c96723fb72d5eaaa23b2c7f36760b371938927
isabelle-np <contact@icnp.design>
2019-02-10 09:14:26 -0500

    fixed styling

--- frontend/scripts/unread.js
+++
@@ -1,7 +0,0 @@
-
-let unread = document.querySelector('#firstConvo');
-let unreadImg = document.querySelector('#firstImg');
-unread.onclick = function() {
-    unread.classList.remove("unread");
-    unreadImg.classList.remove("unreadimg");
-};
